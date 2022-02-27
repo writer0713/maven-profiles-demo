ef7961533e85f9ab1a26b32b156b2560634caf93
tyler.kim <writer0713@naver.com>
2022-02-27 18:45:07 +0900

    feat: 프로젝트 세팅
    - maven 프로젝트 세팅 (with. profile)

---
+++ src/main/java/com/practice/mymaven/MyMavenApplication.java
@@ -0,0 +1,13 @@
+package com.practice.mymaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/mymaven/MyMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mymaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
